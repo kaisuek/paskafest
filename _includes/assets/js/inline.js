038bc84dcb50671477e156872b10027e47811a37
Kaisu K <kaisuek@users.noreply.github.com>
2025-05-31 16:34:58 +0300

    Update inline.js

--- _includes/assets/js/inline.js
+++ _includes/assets/js/inline.js
@@ -1,1 +1,14 @@
 // Add your inline JS here
+
+<script>
+  const gallery = document.getElementById('artistGallery');
+  const scrollAmount = 300; // pikselimäärä per painallus
+
+  document.querySelector('.scroll-btn.left').addEventListener('click', () => {
+    gallery.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
+  });
+
+  document.querySelector('.scroll-btn.right').addEventListener('click', () => {
+    gallery.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+  });
+</script>
